0a2e5eddef033bf512c8ea6bf567af033f6453c2
Cruz <cruz.delacruz37@gmail.com>
2019-05-21 14:02:40 -0400

    added piece that walks througha all folder/files within folder to backup and adds to the zip file

--- backupToZip.py
+++ backupToZip.py
@@ -22,8 +22,18 @@ def  backupToZip(folder):
 	print('Creating %s...' % (zipFilename))
 	backupToZip = zipfile.ZipFile(zipFilename, 'w')
 	
-	# TODO: Walk the entire folder tree and compress the files in each folder
-	
+	# Walk the entire folder tree and compress the files in each folder
+	for foldername, subfolders, filenames in os.walk(folder):
+		print('Adding files in %s...' % (foldername))
+		# Add the current folder to the Zip file.
+		backupZip.write(foldername)
+		# Add all the files in this folder to the ZIP file.
+		for filename in filenames:
+			newBase = os.path.basename(folder) + '_'
+			if filename.startswith(newBase) and filename.endswith('.zip'):
+				continue # don't backup the backup ZIP files
+			backupZip.write(os.path.join(foldername,filename))
+	backupZip.close()
 	print('Done')
 	
 	
