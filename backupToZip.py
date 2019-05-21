76e462cdef3c5c3579960f1247ed7b40fb2b81cb
Cruz <cruz.delacruz37@gmail.com>
2019-05-21 13:48:53 -0400

    added the main program to repo. Just the shell and TODOs

---
+++ backupToZip.py
@@ -0,0 +1,28 @@
+#! python3
+# backupToZip.py - Copies entire folder and its contents into
+# a ZIP file whose filename incremenets
+
+import zipfile, os
+
+def  backupToZip(folder):
+	# BAckup the entire conents of "folder" into a ZIP file.
+	
+	folder = os.path.abspath(folder) # make sure folder is absolute
+	
+	# Figure out the filename this code should use based on
+	# what files already exist.
+	number = 1
+	while True:
+		zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
+		if not os.path.exists(zipFilename):
+			break
+		number += 1
+		
+	# TODO: Create the ZIP file
+	
+	
+	# TODO: Walk the entire folder tree and compress the files in each folder
+	print('Done')
+	
+	
+backupToZip('C:\\Users\\cruz\\Desktop\\Python++')
